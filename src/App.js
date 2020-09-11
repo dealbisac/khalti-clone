0899457a4d72d0f3a6c8e5a402d84d56b8a859fc
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2020-09-11 23:38:40 +0545

    sidebar menu icons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Sidebar from './Sidebar';
+// import Content from './Content';
+// import Wallet from './Wallet';
+
+
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <h1>Hello Khalti Clone</h1>
+
+      {/* Sidebar Menu */}
+      <Sidebar />
+
+      {/* Main Content Box */}
+      {/* <Content /> */}
+
+      {/* Wallet and Info */}
+      {/* <Wallet /> */}
     </div>
   );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './Wallet.css';
+import SidebarRow from './SidebarRow';
+import { Avatar } from '@material-ui/core';
+import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
+import LocalMallOutlinedIcon from '@material-ui/icons/LocalMallOutlined';
+import VerticalSplitIcon from '@material-ui/icons/VerticalSplit';
+import DateRangeIcon from '@material-ui/icons/DateRange';
+import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined';
+import AccountBalanceWalletOutlinedIcon from '@material-ui/icons/AccountBalanceWalletOutlined';
+import AccountBalanceOutlinedIcon from '@material-ui/icons/AccountBalanceOutlined';
+import QueryBuilderOutlinedIcon from '@material-ui/icons/QueryBuilderOutlined';
+import CardGiftcardOutlinedIcon from '@material-ui/icons/CardGiftcardOutlined';
+import LoyaltyOutlinedIcon from '@material-ui/icons/LoyaltyOutlined';
+import SendOutlinedIcon from '@material-ui/icons/SendOutlined';
+import SimCardOutlinedIcon from '@material-ui/icons/SimCardOutlined';
+import MonetizationOnOutlinedIcon from '@material-ui/icons/MonetizationOnOutlined';
+import EqualizerOutlinedIcon from '@material-ui/icons/EqualizerOutlined';
+import HowToVoteIcon from '@material-ui/icons/HowToVote';
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar__info">
+                <Avatar />
+                <strong>Username</strong>
+                <p>chand.dipendra19@gmail.com</p>
+                <p>9843681599</p>
+            </div>
+            <div className="sidebar__menu">
+                <SidebarRow Icon={HomeOutlinedIcon} title="Home" />
+                <SidebarRow Icon={LocalMallOutlinedIcon} title="Bazaar" />
+                <SidebarRow Icon={VerticalSplitIcon} title="Services" />
+                <SidebarRow Icon={DateRangeIcon} title="Bookings" />
+                <SidebarRow Icon={PersonOutlineOutlinedIcon} title="Account" />
+                <SidebarRow Icon={AccountBalanceWalletOutlinedIcon} title="Wallet" />
+                <SidebarRow Icon={AccountBalanceOutlinedIcon} title="Bank Link" />
+                <SidebarRow Icon={QueryBuilderOutlinedIcon} title="Transaction History" />
+                <SidebarRow Icon={CardGiftcardOutlinedIcon} title="Coupon" />
+                <SidebarRow Icon={LoyaltyOutlinedIcon} title="Loyalty" />
+                <SidebarRow Icon={SendOutlinedIcon} title="Referral" />
+                <SidebarRow Icon={SimCardOutlinedIcon} title="Sim" />
+                <SidebarRow Icon={MonetizationOnOutlinedIcon} title="My Earnings" />
+                <SidebarRow Icon={EqualizerOutlinedIcon} title="My Limits" />
+                <SidebarRow Icon={HowToVoteIcon} title="Voting" />
+
+            </div>
+
+        </div>
+    );
+}
+
+export default Sidebar;
